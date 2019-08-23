31c388acfc26524da2bb9923e14e90ec5a95a31e
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2019-08-23 11:16:29 -0700

    Update Detect.py

--- Detect.py
+++ Detect.py
@@ -12,7 +12,7 @@ import os, requests, uuid, json
 key_var_name = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
 if not key_var_name in os.environ:
     raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
-subscriptionKey = os.environ[key_var_name]
+subscription_key = os.environ[key_var_name]
 
 endpoint_var_name = 'TRANSLATOR_TEXT_ENDPOINT'
 if not endpoint_var_name in os.environ:
@@ -25,7 +25,7 @@ path = '/detect?api-version=3.0'
 constructed_url = endpoint + path
 
 headers = {
-    'Ocp-Apim-Subscription-Key': subscriptionKey,
+    'Ocp-Apim-Subscription-Key': subscription_key,
     'Content-type': 'application/json',
     'X-ClientTraceId': str(uuid.uuid4())
 }

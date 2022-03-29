6330e5b99c8c3ed9f82fb483f5a636954b4134f1
Chris Wendt <chris.wendt@outlook.com>
2022-03-28 20:29:46 -0700

    Permit non-ascii output
    Add parameter to permit non-ascii output. Issue #20.

--- Translate.py
+++ Translate.py
@@ -27,7 +27,7 @@ endpoint = os.environ[endpoint_var_name]
 # If you encounter any issues with the base_url or path, make sure
 # that you are using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
 path = '/translate?api-version=3.0'
-params = '&from=en&to=de&to=it'
+params = '&from=en&to=de&to=it&to=zh-Hans'
 constructed_url = endpoint + path + params
 
 headers = {
@@ -44,4 +44,4 @@ body = [{
 request = requests.post(constructed_url, headers=headers, json=body)
 response = request.json()
 
-print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ': ')))
+print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
